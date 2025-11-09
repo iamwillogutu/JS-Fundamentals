b79b69f5c408b59a268a77be86ab39e4a9741cbb
iamwillogutu <iamwill.ogutu@gmail.com>
2025-11-09 22:25:52 +0300

    Update description 2

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+// Get the arguments passed to the script (excluding node and script name)
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+    console.log("No argument");
+} else if (args.length === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
