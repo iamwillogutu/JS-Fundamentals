cbdfb3e14075dcae49788e9c64c14c7747683c10
iamwillogutu <iamwill.ogutu@gmail.com>
2025-11-09 22:57:03 +0300

    Update description 6

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -2,10 +2,7 @@ const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 let output = "";
 for (let i = 0; i < messages.length; i++) {
-    output += messages[i];
-    if (i < messages.length - 1) {
-        output += "\n";
-    }
+    output += messages[i] + "\n";
 }
 
 console.log(output);
